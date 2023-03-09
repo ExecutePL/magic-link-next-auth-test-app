82448725e10e83258b19f2ce395556a826a15a2d
veronikadumalo <dumalo44084@gmail.com>
2023-03-09 16:07:45 +0100

    refactor(credentials provider): refactor provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { Magic } from "@magic-sdk/admin";
+// import { Magic } from "@magic-sdk/admin";
+import { Magic } from "magic-sdk";
 
-const magic = new Magic(process.env.MAGIC_SECRET_KEY);
+const magic =
+  typeof window !== "undefined" &&
+  new Magic(process.env.NEXT_PUBLIC_MAGIC_PUBLISHABLE_KEY || "a");
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -37,13 +44,24 @@ export const authOptions: NextAuthOptions = {
         didToken: { label: "DID Token", type: "text" },
       },
       async authorize(credentials, req) {
-        magic.token.validate(credentials?.didToken || "");
-        const metadata = await magic.users.getMetadataByToken(
-          credentials?.didToken || ""
-        );
-        const user = { id: "1", name: "J Smith", email: metadata.email };
+        const didToken = credentials?.didToken;
 
-        if (user) {
+        const res = await fetch("http://localhost:3000/api/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${didToken || ""}`,
+          },
+        });
+
+        if (res.status === 200) {
+          if (!magic) return null;
+          const userMetadata = await magic?.user?.getMetadata();
+          const user = {
+            id: "jfshgad",
+            userName: "TestUser",
+            email: userMetadata.email,
+          };
           return user;
         } else {
           return null;
